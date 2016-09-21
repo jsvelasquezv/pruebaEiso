b503890cb7d085882265f07fcdfdb0cbb6e9c9b0
jsvelasquezv <juseve200@gmail.com>
2016-09-21 13:58:40 -0500

    changes done

--- pruebaEiso.sql
+++ pruebaEiso.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 21-09-2016 a las 08:03:55
+-- Tiempo de generación: 21-09-2016 a las 20:55:57
 -- Versión del servidor: 10.1.13-MariaDB
 -- Versión de PHP: 5.6.23
 
@@ -23,16 +23,154 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `auth_assignment`
+--
+
+CREATE TABLE `auth_assignment` (
+  `item_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `auth_assignment`
+--
+
+INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
+('Admin', '6', 1474476072),
+('Buyer', '8', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_item`
+--
+
+CREATE TABLE `auth_item` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  `rule_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `data` text COLLATE utf8_unicode_ci,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `auth_item`
+--
+
+INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
+('Admin', 1, 'Admin profile', NULL, NULL, 1474476013, 1474479686),
+('Buyer', 1, 'Allow only to create sales', NULL, NULL, 1474476050, 1474479773),
+('manage-products', 2, 'Permission to manage products', NULL, NULL, 1474475825, 1474475825),
+('manage-roles', 2, 'Allow user acces to the role management section', NULL, NULL, 1474476572, 1474476572),
+('manage-users', 2, 'Permission to manage users', NULL, NULL, 1474475774, 1474475774),
+('see-sales', 2, 'Allow to see all sales', NULL, NULL, 1474479673, 1474479673),
+('sell', 2, 'Permission to allow sell products', NULL, NULL, 1474475748, 1474479723);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_item_child`
+--
+
+CREATE TABLE `auth_item_child` (
+  `parent` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `child` varchar(64) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `auth_item_child`
+--
+
+INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
+('Admin', 'manage-products'),
+('Admin', 'manage-roles'),
+('Admin', 'manage-users'),
+('Admin', 'see-sales'),
+('Admin', 'sell'),
+('Buyer', 'sell');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auth_rule`
+--
+
+CREATE TABLE `auth_rule` (
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci,
+  `created_at` int(11) DEFAULT NULL,
+  `updated_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1474474110),
+('m140506_102106_rbac_init', 1474474330);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` double NOT NULL,
+  `stock` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`product_id`, `name`, `price`, `stock`) VALUES
+(1, 'Crema', 10, 77);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `sales_by_user`
 --
 
 CREATE TABLE `sales_by_user` (
   `sale_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
+  `product_id` int(10) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `sale_date` date NOT NULL,
   `sale_value` double NOT NULL,
   `city` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `sales_by_user`
+--
+
+INSERT INTO `sales_by_user` (`sale_id`, `user_id`, `product_id`, `quantity`, `sale_date`, `sale_value`, `city`) VALUES
+(1, 6, 1, 0, '2016-09-21', 10, 'Restrepo'),
+(2, 6, 1, 10, '2016-04-01', 0, 'aaca'),
+(3, 6, 1, 11, '2016-09-08', 0, 'Restrepo'),
+(4, 6, 1, 1, '2016-08-30', 10, 'cali'),
+(5, 6, 1, 10, '2016-08-30', 100, 'cali'),
+(6, 8, 1, 1, '2016-09-28', 10, 'Cali');
 
 -- --------------------------------------------------------
 
@@ -57,11 +195,51 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `cedula`, `username`, `password`, `email`, `state`, `register_date`, `update_date`, `age`) VALUES
-(6, 12345, 'juseve', '12341234', 'juseve200@gmail.com', 1, '2016-09-21 03:57:48', '2016-09-21 03:57:48', 20);
+(6, 12345, 'juseve', '12341234', 'juseve200@gmail.com', 1, '2016-09-21 03:57:48', '2016-09-21 03:57:48', 20),
+(8, 1116525, 'juseve2', '123456', 'velasquez94@hotmail.com', 1, '2016-09-21 17:22:54', '2016-09-21 17:22:54', 22);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD PRIMARY KEY (`item_name`,`user_id`);
+
+--
+-- Indices de la tabla `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `rule_name` (`rule_name`),
+  ADD KEY `idx-auth_item-type` (`type`);
+
+--
+-- Indices de la tabla `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD PRIMARY KEY (`parent`,`child`),
+  ADD KEY `child` (`child`);
+
+--
+-- Indices de la tabla `auth_rule`
+--
+ALTER TABLE `auth_rule`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indices de la tabla `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
 
 --
 -- Indices de la tabla `sales_by_user`
@@ -83,15 +261,43 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT de la tabla `sales_by_user`
 --
 ALTER TABLE `sales_by_user`
-  MODIFY `sale_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `sale_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `auth_assignment`
+--
+ALTER TABLE `auth_assignment`
+  ADD CONSTRAINT `auth_assignment_ibfk_1` FOREIGN KEY (`item_name`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `auth_item`
+--
+ALTER TABLE `auth_item`
+  ADD CONSTRAINT `auth_item_ibfk_1` FOREIGN KEY (`rule_name`) REFERENCES `auth_rule` (`name`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `auth_item_child`
+--
+ALTER TABLE `auth_item_child`
+  ADD CONSTRAINT `auth_item_child_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
